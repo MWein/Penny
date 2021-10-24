915f33470bde18ca45bdadc150a9616a12144efe
Mwein <mweinberg21@gmail.com>
2021-10-24 15:43:24 -0500

    Added more tests for full coverage

--- utils/nextStrikeDates.test.js
+++ utils/nextStrikeDates.test.js
@@ -24,4 +24,41 @@ describe('nextStrikeDates', () => {
     const result = nextStrikeDates()
     expect(result).toEqual([ '2020-12-25', '2021-01-01', '2021-01-08', '2021-01-15' ])
   })
+
+  // For the sake of 100% test coverage
+  it('Returns the next 4 fridays if today is a sunday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-24T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-10-29', '2021-11-05', '2021-11-12', '2021-11-19' ])
+  })
+  it('Returns the next 4 fridays if today is a monday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-25T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-10-29', '2021-11-05', '2021-11-12', '2021-11-19' ])
+  })
+  it('Returns the next 4 fridays if today is a tuesday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-26T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-10-29', '2021-11-05', '2021-11-12', '2021-11-19' ])
+  })
+  it('Returns the next 4 fridays if today is a wednesday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-27T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-10-29', '2021-11-05', '2021-11-12', '2021-11-19' ])
+  })
+  it('Returns the next 4 fridays if today is a thursday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-28T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-10-29', '2021-11-05', '2021-11-12', '2021-11-19' ])
+  })
+  it('Returns the next 4 fridays if today is a friday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-29T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-11-05', '2021-11-12', '2021-11-19', '2021-11-26' ])
+  })
+  it('Returns the next 4 fridays if today is a saturday', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-30T10:00Z').getTime())
+    const result = nextStrikeDates()
+    expect(result).toEqual([ '2021-11-05', '2021-11-12', '2021-11-19', '2021-11-26' ])
+  })
 })
