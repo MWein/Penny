c334d8238d891cc87d1a7ba81aca6b8606fceab3
Mwein <mweinberg21@gmail.com>
2021-10-24 17:48:18 -0500

    Select best option now supports type, returns full object

--- tradier/selectBestOption.js
+++ tradier/selectBestOption.js
@@ -5,10 +5,10 @@ const selectBest = require('./selectBestOptionForDay')
 const _selectOptionWithBestWeeklyRate = options => options.length > 0 ? options.reduce((acc, option) =>
   option.weeklyRate > acc.weeklyRate ? option : acc,
   options[0]
-).symbol : null
+) : null
 
 
-const selectBestOption = async (symbol, minStrike = null, maxWeeksOut = 4) => {
+const selectBestOption = async (symbol, type, minStrike = null, maxWeeksOut = 4) => {
   const expirationDates = nextStrikeDates(maxWeeksOut)
 
   // Week is used to calculate the weekly rate
@@ -18,7 +18,7 @@ const selectBestOption = async (symbol, minStrike = null, maxWeeksOut = 4) => {
 
   for (let x = 0; x < expirationDates.length; x++) {
     const expiration = expirationDates[x]
-    const bestOption = await selectBest.selectBestStrikeForDay(symbol, 'call', expiration, minStrike)
+    const bestOption = await selectBest.selectBestStrikeForDay(symbol, type, expiration, minStrike)
 
     // Skip if best option is empty object
     if (bestOption.symbol) {
--- tradier/selectBestOption.test.js
+++ tradier/selectBestOption.test.js
@@ -18,7 +18,7 @@ describe('_selectOptionWithBestWeeklyRate', () => {
       { weeklyRate: 25 },
     ]
     const bestOption = _selectOptionWithBestWeeklyRate(chain)
-    expect(bestOption).toEqual()
+    expect(bestOption).toEqual({ weeklyRate: 75 })
   })
 })
 
@@ -28,28 +28,37 @@ describe('selectBestOption', () => {
     selectBest.selectBestStrikeForDay = jest.fn()
   })
 
-  it('If not given optional inputs, no min and 4 expirations', async () => {
+  it('If not given optional inputs, no min and 4 expirations. Passes type; call', async () => {
     selectBest.selectBestStrikeForDay.mockReturnValue({})
-    await selectBestOption('AAPL')
+    await selectBestOption('AAPL', 'call')
     expect(selectBest.selectBestStrikeForDay).toHaveBeenCalledTimes(4)
+    expect(selectBest.selectBestStrikeForDay.mock.calls[0][1]).toEqual('call')
+    expect(selectBest.selectBestStrikeForDay.mock.calls[0][3]).toEqual(null)
+  })
+
+  it('If not given optional inputs, no min and 4 expirations. Passes type; put', async () => {
+    selectBest.selectBestStrikeForDay.mockReturnValue({})
+    await selectBestOption('AAPL', 'put')
+    expect(selectBest.selectBestStrikeForDay).toHaveBeenCalledTimes(4)
+    expect(selectBest.selectBestStrikeForDay.mock.calls[0][1]).toEqual('put')
     expect(selectBest.selectBestStrikeForDay.mock.calls[0][3]).toEqual(null)
   })
 
   it('If not given maxWeeksOut, defaults to 4 expirations', async () => {
     selectBest.selectBestStrikeForDay.mockReturnValue({})
-    await selectBestOption('AAPL', 30)
+    await selectBestOption('AAPL', 'call', 30)
     expect(selectBest.selectBestStrikeForDay).toHaveBeenCalledTimes(4)
   })
 
   it('If given maxWeeksOut, number of calls should match', async () => {
     selectBest.selectBestStrikeForDay.mockReturnValue({})
-    await selectBestOption('AAPL', 30, 6)
+    await selectBestOption('AAPL', 'call', 30, 6)
     expect(selectBest.selectBestStrikeForDay).toHaveBeenCalledTimes(6)
   })
 
   it('Symbol and minStrike is passed to selectBestStrikeForDay', async () => {
     selectBest.selectBestStrikeForDay.mockReturnValue({})
-    await selectBestOption('AAPL', 30, 1)
+    await selectBestOption('AAPL', 'call', 30, 1)
     expect(selectBest.selectBestStrikeForDay.mock.calls[0][0]).toEqual('AAPL')
     // Skipping the date one so I don't have to use fake timers
     expect(selectBest.selectBestStrikeForDay.mock.calls[0][3]).toEqual(30)
@@ -58,7 +67,7 @@ describe('selectBestOption', () => {
   it('Returns null if nothing is returned', async () => {
     selectBest.selectBestStrikeForDay.mockReturnValueOnce({})
     selectBest.selectBestStrikeForDay.mockReturnValueOnce({})
-    const bestOption = await selectBestOption('AAPL', 30, 2)
+    const bestOption = await selectBestOption('AAPL', 'call', 30, 2)
     expect(bestOption).toEqual(null)
   })
 
@@ -71,8 +80,12 @@ describe('selectBestOption', () => {
       symbol: 'AAPL4321',
       premium: 85, // Weekly rate = 42.5
     })
-    const bestOption = await selectBestOption('AAPL', 30, 2)
-    expect(bestOption).toEqual('AAPL1234')
+    const bestOption = await selectBestOption('AAPL', 'call', 30, 2)
+    expect(bestOption).toEqual({
+      symbol: 'AAPL1234',
+      premium: 85,
+      weeklyRate: 85,
+    })
   })
 
   it('Returns the option with the highest weekly rate. Second one', async () => {
@@ -84,7 +97,11 @@ describe('selectBestOption', () => {
       symbol: 'AAPL4321',
       premium: 172, // Weekly rate = 86
     })
-    const bestOption = await selectBestOption('AAPL', 30, 2)
-    expect(bestOption).toEqual('AAPL4321')
+    const bestOption = await selectBestOption('AAPL', 'call', 30, 2)
+    expect(bestOption).toEqual({
+      symbol: 'AAPL4321',
+      premium: 172,
+      weeklyRate: 86,
+    })
   })
 })
