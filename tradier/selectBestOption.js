0a287f456ba39fc5f9b98dc2dc6eab1372322a86
Mwein <mweinberg21@gmail.com>
2021-10-24 14:54:42 -0500

    nextStrikeDates function

---
+++ tradier/selectBestOption.js
@@ -0,0 +1,43 @@
+const { get } = require('../utils/network')
+
+
+const _nextStrikeDates = () => {
+  const date = new Date()
+  const dates = []
+
+  while(dates.length < 4) {
+    switch (date.getDay()) {
+      case 0: date.setDate(date.getDate() + 5)
+      break
+      case 1: date.setDate(date.getDate() + 4)
+      break
+      case 2: date.setDate(date.getDate() + 3)
+      break
+      case 3: date.setDate(date.getDate() + 2)
+      break
+      case 4: date.setDate(date.getDate() + 1)
+      break
+      case 5: date.setDate(date.getDate() + 7) // Off to next friday
+      break
+      case 6: date.setDate(date.getDate() + 6)
+      break
+    }
+    // ISO string returns zulu time and can screw up the date
+    const offset = date.getTimezoneOffset()
+    const actualDate = new Date(date.getTime() - (offset*60*1000))
+    dates.push(actualDate.toISOString().split('T')[0])
+  }
+  
+  return dates
+}
+
+
+const selectBestCall = async (symbol, minStrik) => {
+
+}
+
+
+module.exports = {
+  _nextStrikeDates,
+  selectBestCall,
+}
