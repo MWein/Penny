c499b5d42cf786edab07df0f62d5a8b6533d8ce6
Mwein <mweinberg21@gmail.com>
2021-10-24 15:57:09 -0500

    Tests for format and filter call chain functions

--- tradier/selectBestCall.test.js
+++ tradier/selectBestCall.test.js
@@ -7,12 +7,171 @@ const {
 } = require('./selectBestCall')
 
 
-
 describe('_formatCallChain', () => {
-
-  it('Returns the ', async () => {
-
+  it('Takes the response from Tradier and formats it to be easier for me to read when debugging', async () => {
+    const chain = [
+      {
+        symbol: 'AAPL',
+        bid: 1.07,
+        strike: 1000,
+        greeks: {
+          delta: 0.4
+        },
+        expiration_date: 'tomorrow',
+        option_type: 'call'
+      }
+    ]
+    const formattedCallChain = _formatCallChain(chain)
+    expect(formattedCallChain).toEqual([
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 1000,
+        delta: 0.4,
+        distanceTo30: 0.10000000000000003,
+        expiration: 'tomorrow',
+      }
+    ])
   })
 
+  it('Filters for calls only', async () => {
+    const chain = [
+      {
+        symbol: 'AAPL',
+        bid: 1.07,
+        strike: 1000,
+        greeks: {
+          delta: 0.4
+        },
+        expiration_date: 'tomorrow',
+        option_type: 'call'
+      },
+      {
+        symbol: 'TSLA',
+        bid: 1.09,
+        strike: 80000,
+        greeks: {
+          delta: 0.1
+        },
+        expiration_date: 'day after tomorrow',
+        option_type: 'put'
+      }
+    ]
+    const formattedCallChain = _formatCallChain(chain)
+    expect(formattedCallChain).toEqual([
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 1000,
+        delta: 0.4,
+        distanceTo30: 0.10000000000000003,
+        expiration: 'tomorrow',
+      }
+    ])
+  })
+})
 
+
+describe('_filterCallChain', () => {
+  it('If minStrike is passed, filters out anything lower than the minimum', () => {
+    const chain = [
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 61,
+        delta: 0.3,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 62,
+        delta: 0.3,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 63,
+        delta: 0.3,
+      },
+    ]
+    const filteredChain = _filterCallChain(chain, 63)
+    expect(filteredChain).toEqual([
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 63,
+        delta: 0.3,
+      },
+    ])
+  })
+
+  it('Filters out high and low deltas', () => {
+    const chain = [
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 61,
+        delta: 0.01,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 62,
+        delta: 0.3,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 63,
+        delta: 0.5,
+      },
+    ]
+    const filteredChain = _filterCallChain(chain)
+    expect(filteredChain).toEqual([
+      {
+        symbol: 'AAPL',
+        premium: 107,
+        strike: 62,
+        delta: 0.3,
+      },
+    ])
+  })
+
+  it('Filters out shit premiums', () => {
+    const chain = [
+      {
+        symbol: 'AAPL',
+        premium: 2,
+        strike: 61,
+        delta: 0.3,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 5,
+        strike: 62,
+        delta: 0.3,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 7,
+        strike: 63,
+        delta: 0.3,
+      },
+    ]
+    const filteredChain = _filterCallChain(chain)
+    expect(filteredChain).toEqual([
+      {
+        symbol: 'AAPL',
+        premium: 5,
+        strike: 62,
+        delta: 0.3,
+      },
+      {
+        symbol: 'AAPL',
+        premium: 7,
+        strike: 63,
+        delta: 0.3,
+      },
+    ])
+  })
 })
