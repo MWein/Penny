4882d7d65ea0ddb4d8ba76a914edcef1a4cb985a
Mwein <mweinberg21@gmail.com>
2021-10-24 21:26:31 -0500

    coveredCall opportunity function finished

--- core/coveredCall.js
+++ core/coveredCall.js
@@ -7,53 +7,70 @@ const {
 } = require('../utils/determineOptionType')
 
 
-const _generatePermittedPositionsMap = (optionableStocks, currentOptions, pendingOptions) => {
+// Note: This function assumes that positions were split between stocks and options properly
+//       and that put options were filtered out
+const _generatePermittedPositionsArray = (optionableStocks, currentOptions, pendingOptions) => {
   if (optionableStocks.length === 0) {
-    return {}
+    return []
   }
 
+  // Make the initial map
   const permittedCallsMap = optionableStocks.reduce((acc, stock) => ({
     ...acc,
     [stock.symbol]: Math.floor(stock.quantity / 100)
   }), {})
 
+  // Decrease by 1 for every call option that exists
   currentOptions.map(opt => {
     const underlying = getUnderlying(opt.symbol)
     const quantity = Math.abs(opt.quantity) // Short options are negative
     permittedCallsMap[underlying] = permittedCallsMap[underlying] - quantity
   })
 
+  // Decrease by 1 for every call option order that exists
   pendingOptions.map(opt => {
     const underlying = opt.symbol
     const quantity = Math.abs(opt.quantity) // Short options are negative
     permittedCallsMap[underlying] = permittedCallsMap[underlying] - quantity
   })
 
-  return permittedCallsMap
+  // Format map into an array
+  return Object.keys(permittedCallsMap).reduce((acc, key) => {
+    const quantity = permittedCallsMap[key]
+    return quantity <= 0 ? acc : 
+      [
+        ...acc,
+        {
+          symbol: key,
+          quantity,
+        }
+      ]
+    }, [])
 }
 
 
-const sellCoveredCalls = async () => {
+const _determineCoverableTickers = async () => {
   const positions = await position.getPositions()
   const optionableStocks = positions.filter(pos => pos.quantity >= 100 && !isOption(pos.symbol))
   if (optionableStocks.length === 0) {
-    return 'no available positions'
+    return []
   }
   const currentOptions = positions.filter(pos => isOption(pos.symbol))
 
   const orders = await order.getOrders()
   const callOrders = orders.filter(ord => ord.class === 'option' && ['pending', 'open'].includes(ord.status))
-    .filter(ord => determineOptionTypeFromSymbol(ord.symbol) === 'call')
+    .filter(ord => determineOptionTypeFromSymbol(ord.option_symbol) === 'call')
 
-  
+  return _generatePermittedPositionsArray(optionableStocks, currentOptions, callOrders)
+}
 
 
-  // get current orders
-
+const sellCoveredCalls = async () => {
 
 }
 
 module.exports = {
-  _generatePermittedPositionsMap,
+  _generatePermittedPositionsArray,
+  _determineCoverableTickers,
   sellCoveredCalls,
 }
--- core/coveredCall.test.js
+++ core/coveredCall.test.js
@@ -2,22 +2,22 @@ const positions = require('../tradier/getPositions')
 const orders = require('../tradier/getOrders')
 
 const {
-  _generatePermittedPositionsMap,
-  sellCoveredCalls,
+  _generatePermittedPositionsArray,
+  _determineCoverableTickers,
 } = require('./coveredCall')
 
 
 
-describe('_generatePermittedPositionsMap', () => {
-  it('Returns empty object if there are no optionable stocks', () => {
+describe('_generatePermittedPositionsArray', () => {
+  it('Returns empty array if there are no optionable stocks', () => {
     const optionableStocks = []
     const currentOptions = []
     const pendingOptions = []
-    const map = _generatePermittedPositionsMap(optionableStocks, currentOptions, pendingOptions)
-    expect(map).toEqual({})
+    const map = _generatePermittedPositionsArray(optionableStocks, currentOptions, pendingOptions)
+    expect(map).toEqual([])
   })
 
-  it('Returns map of stocks with their permitted call number; no current or pending options', () => {
+  it('Returns array of stocks with their permitted call number; no current or pending options', () => {
     const optionableStocks = [
       {
         cost_basis: 207.01,
@@ -50,16 +50,24 @@ describe('_generatePermittedPositionsMap', () => {
     ]
     const currentOptions = []
     const pendingOptions = []
-    const map = _generatePermittedPositionsMap(optionableStocks, currentOptions, pendingOptions)
-    expect(map).toEqual({
-      AAPL: 1,
-      AMZN: 1,
-      CAH: 2,
-      FB: 0
-    })
+    const result = _generatePermittedPositionsArray(optionableStocks, currentOptions, pendingOptions)
+    expect(result).toEqual([
+      {
+        symbol: 'AAPL',
+        quantity: 1,
+      },
+      {
+        symbol: 'AMZN',
+        quantity: 1,
+      },
+      {
+        symbol: 'CAH',
+        quantity: 2,
+      },
+    ])
   })
 
-  it('Returns map of stocks with their permitted call number; no pending options', () => {
+  it('Returns array of stocks with their permitted call number; no pending options', () => {
     const optionableStocks = [
       {
         cost_basis: 207.01,
@@ -107,16 +115,20 @@ describe('_generatePermittedPositionsMap', () => {
       }
     ]
     const pendingOptions = []
-    const map = _generatePermittedPositionsMap(optionableStocks, currentOptions, pendingOptions)
-    expect(map).toEqual({
-      AAPL: 0,
-      AMZN: 1,
-      CAH: 1,
-      FB: 0
-    })
+    const result = _generatePermittedPositionsArray(optionableStocks, currentOptions, pendingOptions)
+    expect(result).toEqual([
+      {
+        symbol: 'AMZN',
+        quantity: 1,
+      },
+      {
+        symbol: 'CAH',
+        quantity: 1,
+      },
+    ])
   })
 
-  it('Returns map of stocks with permitted call number; no current options', () => {
+  it('Returns array of stocks with permitted call number; no current options', () => {
     const optionableStocks = [
       {
         cost_basis: 207.01,
@@ -186,16 +198,16 @@ describe('_generatePermittedPositionsMap', () => {
         option_symbol: 'CAH180720C00274000'
       },
     ]
-    const map = _generatePermittedPositionsMap(optionableStocks, currentOptions, pendingOptions)
-    expect(map).toEqual({
-      AAPL: 0,
-      AMZN: 1,
-      CAH: 0,
-      FB: 0
-    })
+    const result = _generatePermittedPositionsArray(optionableStocks, currentOptions, pendingOptions)
+    expect(result).toEqual([
+      {
+        symbol: 'AMZN',
+        quantity: 1,
+      },
+    ])
   })
 
-  it('Returns map of stocks with permitted call number', () => {
+  it('Returns array of stocks with permitted call number', () => {
     const optionableStocks = [
       {
         cost_basis: 207.01,
@@ -273,25 +285,19 @@ describe('_generatePermittedPositionsMap', () => {
         option_symbol: 'CAH180720C00274000'
       },
     ]
-    const map = _generatePermittedPositionsMap(optionableStocks, currentOptions, pendingOptions)
-    expect(map).toEqual({
-      AAPL: 0,
-      AMZN: 0,
-      CAH: 0,
-      FB: 0
-    })
+    const result = _generatePermittedPositionsArray(optionableStocks, currentOptions, pendingOptions)
+    expect(result).toEqual([])
   })
 })
 
 
-
-describe('sellCoveredCalls', () => {
+describe('_determineCoverableTickers', () => {
   beforeEach(() => {
     positions.getPositions = jest.fn()
     orders.getOrders = jest.fn()
   })
 
-  it('Returns \'no available positions\' if there are no optionable positions', async () => {
+  it('Returns \'no available positions reason 1\' if there are no optionable positions', async () => {
     positions.getPositions.mockReturnValue([
       {
         cost_basis: 207.01,
@@ -308,9 +314,131 @@ describe('sellCoveredCalls', () => {
         symbol: 'TSLA',
       }
     ])
-    const status = await sellCoveredCalls()
-    expect(status).toEqual('no available positions')
+    const status = await _determineCoverableTickers()
+    expect(status).toEqual([])
   })
 
+  it('Returns \'no available positions; reason 2\' if the position map comes up all zeros', async () => {
+    positions.getPositions.mockReturnValue([
+      {
+        cost_basis: 207.01,
+        date_acquired: '2018-08-08T14:41:11.405Z',
+        id: 130089,
+        quantity: 100.00000000,
+        symbol: 'AAPL'
+      },
+      {
+        cost_basis: 1870.70,
+        date_acquired: '2018-08-08T14:42:00.774Z',
+        id: 130090,
+        quantity: 200.00000000,
+        symbol: 'TSLA',
+      },
+      {
+        cost_basis: 1870.70,
+        date_acquired: '2018-08-08T14:42:00.774Z',
+        id: 130090,
+        quantity: -1.00000000,
+        symbol: 'TSLA1234C4321',
+      },
+    ])
+    orders.getOrders.mockReturnValue([
+      {
+        id: 228749,
+        type: 'market',
+        symbol: 'TSLA',
+        side: 'sell_to_open',
+        quantity: 1.00000000,
+        status: 'pending',
+        duration: 'pre',
+        avg_fill_price: 0.00000000,
+        exec_quantity: 0.00000000,
+        last_fill_price: 0.00000000,
+        last_fill_quantity: 0.00000000,
+        remaining_quantity: 0.00000000,
+        create_date: '2018-06-06T20:16:17.342Z',
+        transaction_date: '2018-06-06T20:16:17.357Z',
+        class: 'option',
+        option_symbol: 'TSLA180720C00274000'
+      },
+      {
+        id: 228749,
+        type: 'market',
+        symbol: 'AAPL',
+        side: 'sell_to_open',
+        quantity: 1.00000000,
+        status: 'pending',
+        duration: 'pre',
+        avg_fill_price: 0.00000000,
+        exec_quantity: 0.00000000,
+        last_fill_price: 0.00000000,
+        last_fill_quantity: 0.00000000,
+        remaining_quantity: 0.00000000,
+        create_date: '2018-06-06T20:16:17.342Z',
+        transaction_date: '2018-06-06T20:16:17.357Z',
+        class: 'option',
+        option_symbol: 'AAPL180720C00274000'
+      },
+    ])
+
+    const result = await _determineCoverableTickers()
+
+    expect(result).toEqual([])
+  })
+
+  it('Returns list of coverable tickers', async () => {
+    positions.getPositions.mockReturnValue([
+      {
+        cost_basis: 207.01,
+        date_acquired: '2018-08-08T14:41:11.405Z',
+        id: 130089,
+        quantity: 100.00000000,
+        symbol: 'AAPL'
+      },
+      {
+        cost_basis: 1870.70,
+        date_acquired: '2018-08-08T14:42:00.774Z',
+        id: 130090,
+        quantity: 200.00000000,
+        symbol: 'TSLA',
+      },
+      {
+        cost_basis: 1870.70,
+        date_acquired: '2018-08-08T14:42:00.774Z',
+        id: 130090,
+        quantity: -1.00000000,
+        symbol: 'TSLA1234C4321',
+      },
+    ])
+    orders.getOrders.mockReturnValue([
+      {
+        id: 228749,
+        type: 'market',
+        symbol: 'AAPL',
+        side: 'sell_to_open',
+        quantity: 1.00000000,
+        status: 'pending',
+        duration: 'pre',
+        avg_fill_price: 0.00000000,
+        exec_quantity: 0.00000000,
+        last_fill_price: 0.00000000,
+        last_fill_quantity: 0.00000000,
+        remaining_quantity: 0.00000000,
+        create_date: '2018-06-06T20:16:17.342Z',
+        transaction_date: '2018-06-06T20:16:17.357Z',
+        class: 'option',
+        option_symbol: 'AAPL180720C00274000'
+      },
+    ])
+
+    const result = await _determineCoverableTickers()
+
+    expect(result).toEqual([
+      {
+        symbol: 'TSLA',
+        quantity: 1
+      },
+    ])
+  })
 
 })
