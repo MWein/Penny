215eaebb5cf519c7fa3d3de87f083aa9d4221e94
Mwein <mweinberg21@gmail.com>
2021-10-25 22:37:49 -0500

    generateOrderObject and tests

--- utils/testHelpers.test.js
+++ utils/testHelpers.test.js
@@ -1,11 +1,215 @@
 const {
+  _generateSymbol,
   generateOrderObject,
   generatePositionObject,
 } = require('./testHelpers')
 
 
-describe('generateOrderObject', () => {
+// {
+//   id: 228749,
+//   type: 'market',
+//   symbol: 'AAPL',
+//   side: 'sell_to_open',
+//   quantity: 1.00000000,
+//   status: 'pending',
+//   duration: 'pre',
+//   avg_fill_price: 0.00000000,
+//   exec_quantity: 0.00000000,
+//   last_fill_price: 0.00000000,
+//   last_fill_quantity: 0.00000000,
+//   remaining_quantity: 0.00000000,
+//   create_date: '2018-06-06T20:16:17.342Z',
+//   transaction_date: '2018-06-06T20:16:17.357Z',
+//   class: 'option',
+//   option_symbol: 'AAPL180720C00274000'
+// }
 
+
+describe('_generateSymbol', () => {
+  it('Returns the symbol if type is stock', () => {
+    expect(_generateSymbol('AAPL', 'stock')).toEqual('AAPL')
+  })
+
+  it('Returns call symbol', () => {
+    expect(_generateSymbol('TSLA', 'call')).toEqual('TSLA1234C3214')
+  })
+
+  it('Returns put symbol', () => {
+    expect(_generateSymbol('FB', 'put')).toEqual('FB1234P3214')
+  })
+})
+
+
+describe('generateOrderObject', () => {
+  it('Generates a order object if only given symbol param', () => {
+    const order = generateOrderObject('AAPL')
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'AAPL',
+      side: 'sell_to_open',
+      quantity: 1,
+      status: 'pending',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'equity',
+    })
+  })
+
+  it('Generates a order object with correct quantity', () => {
+    const order = generateOrderObject('AAPL', 4)
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'AAPL',
+      side: 'sell_to_open',
+      quantity: 4,
+      status: 'pending',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'equity',
+    })
+  })
+
+  it('Generates a order object with type stock', () => {
+    const order = generateOrderObject('TSLA', 7, 'stock')
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'TSLA',
+      side: 'sell_to_open',
+      quantity: 7,
+      status: 'pending',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'equity',
+    })
+  })
+
+  it('Generates a order object with type call', () => {
+    const order = generateOrderObject('TSLA', 7, 'call')
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'TSLA',
+      side: 'sell_to_open',
+      quantity: 7,
+      status: 'pending',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'option',
+      option_symbol: 'TSLA1234C3214'
+    })
+  })
+
+  it('Generates a order object with type put', () => {
+    const order = generateOrderObject('FB', -2, 'put')
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'FB',
+      side: 'sell_to_open',
+      quantity: -2,
+      status: 'pending',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'option',
+      option_symbol: 'FB1234P3214'
+    })
+  })
+
+  it('Generates a order object with correct side', () => {
+    const order = generateOrderObject('FB', 2, 'stock', 'buy_to_close')
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'FB',
+      side: 'buy_to_close',
+      quantity: 2,
+      status: 'pending',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'equity',
+    })
+  })
+
+  it('Generates a order object with correct status', () => {
+    const order = generateOrderObject('FB', 2, 'stock', 'buy_to_close', 'open')
+    expect(order).toEqual({
+      id: 123456,
+      type: 'market',
+      symbol: 'FB',
+      side: 'buy_to_close',
+      quantity: 2,
+      status: 'open',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'equity',
+    })
+  })
+
+  it('Generates a order object with correct id', () => {
+    const order = generateOrderObject('FB', 2, 'stock', 'buy_to_close', 'open', 654321)
+    expect(order).toEqual({
+      id: 654321,
+      type: 'market',
+      symbol: 'FB',
+      side: 'buy_to_close',
+      quantity: 2,
+      status: 'open',
+      duration: 'pre',
+      avg_fill_price: 0.00000000,
+      exec_quantity: 0.00000000,
+      last_fill_price: 0.00000000,
+      last_fill_quantity: 0.00000000,
+      remaining_quantity: 0.00000000,
+      create_date: '2018-06-06T20:16:17.342Z',
+      transaction_date: '2018-06-06T20:16:17.357Z',
+      class: 'equity',
+    })
+  })
 })
 
 
