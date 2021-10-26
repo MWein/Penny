3108587e6b93dc18d7a4c5221411abe980d0e15c
Mwein <mweinberg21@gmail.com>
2021-10-26 12:45:07 -0500

    Refactored nakedPut

--- core/nakedPut.js
+++ core/nakedPut.js
@@ -1,14 +1,32 @@
+const bestOption = require('../tradier/selectBestOption')
 const watchlistUtil = require('../tradier/watchlist')
 const nakedPutHelpers = require('./nakedPutCycle')
+
 
 // Broke into its own file so I can stub the cycle function
 const sellNakedPuts = async () => {
   const watchlist = await watchlistUtil.getWatchlistSymbols()
+  if (watchlist.length === 0) {
+    return
+  }
+
+  // for-loop so it doesn't call all at once
+  const bestOptions = []
+  for (let x = 0; x < watchlist.length; x++) {
+    const symbol = watchlist[x]
+    const best = await bestOption.selectBestOption(symbol, 'put')
+    if (best && best.strike * 100 < process.env.MAXALLOCATION) {
+      bestOptions.push(best)
+    }
+  }
+  if (bestOptions.length === 0) {
+    return
+  }
 
   // Cycle until cycle function returns something other than 'success'
   const _sellNakedPutsHelper = async () => {
     console.log('Selling Puts')
-    const result = await nakedPutHelpers.sellNakedPutsCycle(watchlist)
+    const result = await nakedPutHelpers.sellNakedPutsCycle(watchlist, bestOptions)
     console.log(result)
     if (result === 'success') {
       await _sellNakedPutsHelper()
--- core/nakedPut.test.js
+++ core/nakedPut.test.js
@@ -1,3 +1,10 @@
+const balanceUtil = require('../tradier/getBalances')
+const priceUtil = require('../tradier/getPrices')
+const positionUtil = require('../tradier/getPositions')
+const orderUtil = require('../tradier/getOrders')
+const bestOption = require('../tradier/selectBestOption')
+const { getUnderlying } = require('../utils/determineOptionType')
+const sendOrdersUtil = require('../tradier/sendOrders')
 const watchlistUtil = require('../tradier/watchlist')
 const nakedPutHelpers = require('./nakedPutCycle')
 
@@ -9,25 +16,63 @@ describe('sellNakedPuts', () => {
   beforeEach(() => {
     watchlistUtil.getWatchlistSymbols = jest.fn()
     nakedPutHelpers.sellNakedPutsCycle = jest.fn()
+    bestOption.selectBestOption = jest.fn()
   })
 
-  it('Calls cycle function at least once; exits if cycle function returns something other than success', async () => {
+  it('Does nothing if watchlist is empty', async () => {
+    watchlistUtil.getWatchlistSymbols.mockReturnValue([])
+    await sellNakedPuts()
+    expect(bestOption.selectBestOption).not.toHaveBeenCalled()
+    expect(nakedPutHelpers.sellNakedPutsCycle).not.toHaveBeenCalled()
+  })
+
+  it('Retreives the best options for each ticker in the watchlist; does not call cycle function if all nulls', async () => {
+    watchlistUtil.getWatchlistSymbols.mockReturnValue([ 'AAPL', 'BB', 'SFIX' ])
+    bestOption.selectBestOption.mockReturnValue(null)
+    await sellNakedPuts()
+    expect(bestOption.selectBestOption).toHaveBeenCalledTimes(3)
+    expect(bestOption.selectBestOption).toHaveBeenCalledWith('AAPL', 'put')
+    expect(bestOption.selectBestOption).toHaveBeenCalledWith('BB', 'put')
+    expect(bestOption.selectBestOption).toHaveBeenCalledWith('BB', 'put')
+    expect(nakedPutHelpers.sellNakedPutsCycle).not.toHaveBeenCalled()
+  })
+
+  it('Retreives the best options for each ticker in the watchlist; does not call cycle function if all above max allocation', async () => {
+    process.env.MAXALLOCATION = 999
+    watchlistUtil.getWatchlistSymbols.mockReturnValue([ 'AAPL', 'BB', 'SFIX' ])
+    bestOption.selectBestOption.mockReturnValue({ strike: 10 })
+    await sellNakedPuts()
+    expect(bestOption.selectBestOption).toHaveBeenCalledTimes(3)
+    expect(bestOption.selectBestOption).toHaveBeenCalledWith('AAPL', 'put')
+    expect(bestOption.selectBestOption).toHaveBeenCalledWith('BB', 'put')
+    expect(bestOption.selectBestOption).toHaveBeenCalledWith('BB', 'put')
+    expect(nakedPutHelpers.sellNakedPutsCycle).not.toHaveBeenCalled()
+  })
+
+  it('Calls the cycle function at least once with only the options that are below max allocation and are not null; exits cycle function if something other than \'success\' is returned', async () => {
+    process.env.MAXALLOCATION = 1001
     watchlistUtil.getWatchlistSymbols.mockReturnValue([ 'AAPL', 'WMT' ])
+    bestOption.selectBestOption.mockReturnValueOnce({ strike: 10 })
+    bestOption.selectBestOption.mockReturnValueOnce(null)
+    bestOption.selectBestOption.mockReturnValueOnce({ strike: 1002 })
     nakedPutHelpers.sellNakedPutsCycle.mockReturnValue('oh no')
     await sellNakedPuts()
-    expect(watchlistUtil.getWatchlistSymbols).toHaveBeenCalledTimes(1)
     expect(nakedPutHelpers.sellNakedPutsCycle).toHaveBeenCalledTimes(1)
-    expect(nakedPutHelpers.sellNakedPutsCycle).toHaveBeenCalledWith([ 'AAPL', 'WMT' ])
+    expect(nakedPutHelpers.sellNakedPutsCycle).toHaveBeenCalledWith([ 'AAPL', 'WMT' ], [ { strike: 10 } ])
   })
 
   it('Calls cycle function again if success returned', async () => {
+    process.env.MAXALLOCATION = 1001
     watchlistUtil.getWatchlistSymbols.mockReturnValue([ 'AAPL', 'WMT' ])
+    bestOption.selectBestOption.mockReturnValueOnce({ strike: 10 })
+    bestOption.selectBestOption.mockReturnValueOnce(null)
+    bestOption.selectBestOption.mockReturnValueOnce({ strike: 1002 })
     nakedPutHelpers.sellNakedPutsCycle.mockReturnValueOnce('success')
     nakedPutHelpers.sellNakedPutsCycle.mockReturnValueOnce('success')
     nakedPutHelpers.sellNakedPutsCycle.mockReturnValueOnce('oh no')
     await sellNakedPuts()
     expect(watchlistUtil.getWatchlistSymbols).toHaveBeenCalledTimes(1)
     expect(nakedPutHelpers.sellNakedPutsCycle).toHaveBeenCalledTimes(3)
-    expect(nakedPutHelpers.sellNakedPutsCycle).toHaveBeenCalledWith([ 'AAPL', 'WMT' ])
+    expect(nakedPutHelpers.sellNakedPutsCycle).toHaveBeenCalledWith([ 'AAPL', 'WMT' ], [ { strike: 10 } ])
   })
 })
