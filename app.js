33e49f7220d56694f0165d3eaece857be29901c3
Mwein <mweinberg21@gmail.com>
2021-10-27 01:03:19 -0500

    Removed a crapload of manual test comments

--- app.js
+++ app.js
@@ -23,45 +23,10 @@ const { createGTCOrders } = require('./core/gtcOrders')
 
 
 const launch = async () => {
-  // Manual tests
-  //const coverableTickers = await _determineCoverableTickers()
-  //console.log(coverableTickers)
-  // TODO After 9am local time, this should be empty
-
-
-  // createGTCOrders()
+  //createGTCOrders()
   // Comment out the part where it actually buys stuff
   // In createGTCOrders function, oldOptionsPositions should be empty after 7am local time
 
-
-  //sellNakedPuts()
-  // Should sell TTD, ATVI, JD, IRBT
-
-  //await createGTCOrders()
-
-  //const coverableTickers = await _determineCoverableTickers()
-  //console.log(coverableTickers)
-  // Tomorrow it should have TSLA COKE and PINS
-
-  //const best = await selectBestOption('ZNGA', 'call')
-  //console.log(best)
-
-  //const result = await sellCoveredCalls()
-  //console.log(result)
-
-  // TODO other test cases
-  // 1. Manually sell a covered call against TSLA at market open and run again. Should say COKE PINS - DONE
-  // 2. Put an order in after market close for COKE. Should only say PINS
-
-  //const positions = await getPositions()
-  //console.log(positions)
-  // Tomorrow it should have TSLA COKE and PINS
-
-  //const balances = await getBalances()
-  //console.log(balances)
-
-  //const watchlist = await getWatchlistSymbols()
-  //console.log(watchlist)
 
   new CronJob('0 0 8 * * 1-5', () => {
     console.log('Creating GTC Orders')
