a9cda1deee15d9ca751a320f7168519a34d50c33
Mwein <mweinberg21@gmail.com>
2021-10-26 23:59:36 -0500

    Initial settings function

---
+++ utils/settings.js
@@ -0,0 +1,18 @@
+const defaultSettings = {
+  callsEnabled: true,
+  putsEnabled: true,
+  maxAllocation: 1000, // The maximum amount of money to put down on a single ticker
+  reserve: 0, // Money that Penny shouldn't touch. BuyingPower - Reserve. For planned withdrawals.
+  gtcAmount: 1, // Limit price when making automated buy_to_close orders
+}
+
+const getSetting = async key => {
+  // TODO Grab setting from database
+
+  return defaultSettings[key]
+}
+
+
+module.exports = {
+  getSetting,
+}
