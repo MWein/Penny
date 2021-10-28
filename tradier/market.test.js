42bbb11e7e87b5c50bf89974c9cd0d1990feb1f1
Mwein <mweinberg21@gmail.com>
2021-10-28 00:00:27 -0500

    Removed unneeded stuff

--- tradier/market.test.js
+++ tradier/market.test.js
@@ -5,7 +5,7 @@ describe('isMarketOpen', () => {
   beforeEach(() => {
     network.get = jest.fn()
   })
-  
+
   it('Returns false if error is thrown', async () => {
     network.get.mockImplementation(() => {
       throw new Error('NOOOOOOO')
@@ -17,12 +17,7 @@ describe('isMarketOpen', () => {
   it('Returns false if response.state is not \'open\'', async () => {
     network.get.mockReturnValue({
       clock: {
-        date: '2019-05-06',
-        description: 'Market is open from 09:30 to 16:00',
         state: 'postmarket',
-        timestamp: 1557156988,
-        next_change: '16:00',
-        next_state: 'pre'
       }
     })
     const result = await isMarketOpen()
@@ -32,12 +27,7 @@ describe('isMarketOpen', () => {
   it('Returns true if response.state is \'open\'', async () => {
     network.get.mockReturnValue({
       clock: {
-        date: '2019-05-06',
-        description: 'Market is open from 09:30 to 16:00',
         state: 'open',
-        timestamp: 1557156988,
-        next_change: '16:00',
-        next_state: 'postmarket'
       }
     })
     const result = await isMarketOpen()
