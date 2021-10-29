a4a01aa46effd0ad2191ae30b062f9482e563649
Mwein <mweinberg21@gmail.com>
2021-10-29 17:21:04 -0500

    Finished tests

--- utils/log.test.js
+++ utils/log.test.js
@@ -47,3 +47,69 @@ describe('_logWithObject', () => {
     expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
   })
 })
+
+
+describe('_logWithMessage', () => {
+  const originalConsoleLog = console.log
+  let saveFunc
+
+  beforeEach(() => {
+    saveFunc = jest.fn()
+    logSchema.mockReturnValue({
+      save: saveFunc,
+    })
+    console.log = jest.fn()
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+  })
+
+  it('On failure, console logs', async () => {
+    logSchema.mockImplementation(() => {
+      throw new Error('Oh no!!!!!!')
+    })
+    await _logWithMessage('something')
+    expect(logSchema).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('Error reaching database')
+  })
+
+  it('On success, saves and console logs the message', async () => {
+    await _logWithMessage('something')
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logSchema).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
+  })
+})
+
+
+describe('log', () => {
+  const originalConsoleLog = console.log
+  let saveFunc
+
+  beforeEach(() => {
+    saveFunc = jest.fn()
+    logSchema.mockReturnValue({
+      save: saveFunc,
+    })
+    console.log = jest.fn()
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+  })
+
+  it('Calls correct function if given an object', async () => {
+    await log({ type: 'checkin', message: 'something' })
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logSchema).toHaveBeenCalledWith({ type: 'checkin', message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('checkin', ':', 'something')
+  })
+
+  it('Calls correct function if given a string', async () => {
+    await log('something')
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logSchema).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
+  })
+})
