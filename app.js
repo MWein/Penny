f84e43d35b752e19e99f713db61a769b06b76f10
Mwein <mweinberg21@gmail.com>
2021-11-01 16:42:15 -0500

    Removed old stuff

--- app.js
+++ app.js
@@ -2,26 +2,9 @@ require('dotenv').config()
 const CronJob = require('cron').CronJob
 const mongoose = require('mongoose')
 
-//const { getWatchlistSymbols } = require('./tradier/watchlist')
-//const { selectBestOption } = require('./tradier/selectBestOption')
-//const { getPrices } = require('./tradier/getPrices')
-const ordersUtil = require('./tradier/getOrders')
-const { getPositions } = require('./tradier/getPositions')
-//const { sellToOpen } = require('./tradier/sendOrders')
-//const { getBalances } = require('./tradier/getBalances')
-//const { isMarketOpen } = require('./tradier/market')
-const { getGainLoss } = require('./tradier/getGainLoss')
-
+const { sellCoveredCalls } = require('./core/coveredCall')
 const { updateGainLossCollection } = require('./utils/updateGainLoss')
-
-// Permanent imports
-const {
-  //_determineCoverableTickers,
-  sellCoveredCalls
-} = require('./core/coveredCall')
-
 const { sellNakedPuts } = require('./core/nakedPut')
-
 const { createGTCOrders } = require('./core/gtcOrders')
 const { log, clearOldLogs } = require('./utils/log')
 
@@ -70,6 +53,7 @@ const launchCrons = async () => {
   }, null, true, 'America/New_York')
 
   // Run every day at 4:10 NY time
+  // 10 mins after market close
   new CronJob('0 10 16 * * *', () => {
     housekeeping()
   }, null, true, 'America/New_York')
