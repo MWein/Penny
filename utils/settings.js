043b032661c579326a69628ad5cd0ac15dd84b7e
Mwein <mweinberg21@gmail.com>
2021-11-01 11:02:51 -0500

    I am a stupid idiot

--- utils/settings.js
+++ utils/settings.js
@@ -5,7 +5,7 @@ const defaultSettings = {
   putsEnabled: true,
   maxAllocation: 4000, // The maximum amount of money to put down on a single ticker
   reserve: 0, // Money that Penny shouldn't touch. BuyingPower - Reserve. For planned withdrawals.
-  buyToCloseAmount: 1, // Limit price when making automated buy_to_close orders
+  buyToCloseAmount: 0.01, // Limit price when making automated buy_to_close orders
 }
 
 const getSettings = async () => {
