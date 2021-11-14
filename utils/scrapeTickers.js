d869ba388f427eeb8c6563807fc753bc887a16cd
Mwein <mweinberg21@gmail.com>
2021-11-14 16:38:09 -0600

    Replaced console logs with db logs

--- utils/scrapeTickers.js
+++ utils/scrapeTickers.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer')
+const { log } = require('./utils/log')
+
 
 const scrapeTickers = async (tries = 0) => {
   if (tries > 5) {
-    console.log('Too many failed attempts')
+    log({
+      type: 'error',
+      message: 'Too many failed scrape attempts. Aborting.'
+    })
     return []
   }
 
@@ -10,18 +15,18 @@ const scrapeTickers = async (tries = 0) => {
   const page = await browser.newPage()
 
   try {
-    console.log('Navigating to login page')
+    log('Navigating to login page')
     await page.goto(process.env.SCRAPE_TARGETPAGE)
 
-    console.log('Entering creds')
+    log('Entering creds')
     await page.type(process.env.SCRAPE_FIELD1, process.env.SCRAPE_VALUE1)
     await page.type(process.env.SCRAPE_FIELD2, process.env.SCRAPE_VALUE2)
 
-    console.log('Clicking login button')
+    log('Clicking login button')
     await page.click(process.env.SCRAPE_BUTTONID)
     await page.waitForNavigation()
 
-    console.log('Scraping tickers')
+    log('Scraping tickers')
     const scrapedTickers = await page.evaluate(() =>
       Array.from(
         // eslint-disable-next-line no-undef
@@ -30,13 +35,15 @@ const scrapeTickers = async (tries = 0) => {
       )
     )
 
-    console.log('Closing browser')
+    log('Closing browser')
     await browser.close()
 
     return scrapedTickers.map(x => x.trim())
   } catch (e) {
-    console.log(e)
-    console.log('Something went wrong, trying again')
+    log({
+      type: 'error',
+      message: 'Something went wrong, trying again'
+    })
     await browser.close()
     const result = await scrapeTickers(tries + 1)
     return result
