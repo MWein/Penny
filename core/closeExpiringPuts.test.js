a229a819a1aeb41cf44a096a8819ea287e64c7bf
Mwein <mweinberg21@gmail.com>
2021-12-28 03:37:26 -0600

    Filter for puts test

---
+++ core/closeExpiringPuts.test.js
@@ -0,0 +1,82 @@
+const positionsUtil = require('../tradier/getPositions')
+const pricesUtil = require('../tradier/getPrices')
+const quotesUtil = require('../tradier/getQuotes')
+const ordersUtil = require('../tradier/getOrders')
+const sendOrdersUtil = require('../tradier/sendOrders')
+const { getUnderlying } = require('../utils/determineOptionType')
+
+const {
+  _getPutsExpiringToday,
+  _filterForPutsAtProfit,
+  closeExpiringPuts
+} = require('./closeExpiringPuts')
+
+
+// describe('_getPutsExpiringToday', () => {
+
+// })
+
+
+describe('_filterForPutsAtProfit', () => {
+  it('Filters for puts at a profit... duh', () => {
+    const mockPuts = [
+      {
+        symbol: 'ZNGA1234',
+        cost_basis: -42,
+        quantity: -2,
+        price: 0.25
+      },
+      {
+        symbol: 'AAPL1234',
+        cost_basis: -25,
+        quantity: -1,
+        price: 0.02
+      },
+      {
+        symbol: 'TSLA1234',
+        cost_basis: -80,
+        quantity: -1,
+        price: 0.81
+      },
+      {
+        symbol: 'TTD1234',
+        cost_basis: -80,
+        quantity: -2,
+        price: 0.41
+      },
+      {
+        symbol: 'BB1234',
+        cost_basis: -80,
+        quantity: -2,
+        price: 0.39
+      },
+    ]
+
+    const result = _filterForPutsAtProfit(mockPuts)
+
+    expect(result).toEqual([
+      {
+        symbol: 'AAPL1234',
+        cost_basis: -25,
+        quantity: -1,
+        price: 0.02
+      },
+      {
+        symbol: 'BB1234',
+        cost_basis: -80,
+        quantity: -2,
+        price: 0.39
+      },
+    ])
+  })
+})
+
+
+// describe('_closeExistingBTCOrders', () => {
+  
+// })
+
+
+// describe('closeExpiringPuts', () => {
+  
+// })
