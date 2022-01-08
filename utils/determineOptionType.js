6bb9ec027b88acd8202e4704d005c4cf66aa0aac
Mwein <mweinberg21@gmail.com>
2022-01-08 01:48:07 -0600

    Added getExpiration and getStrike functions

--- utils/determineOptionType.js
+++ utils/determineOptionType.js
@@ -1,5 +1,6 @@
 const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
+
 const determineOptionTypeFromSymbol = symbol => {
   if (!isOption(symbol)) {
     return 'neither'
@@ -13,13 +14,33 @@ const determineOptionTypeFromSymbol = symbol => {
   }
 }
 
+
 const isOption = symbol => symbol.split('').some(char => nums.includes(char))
+
 
 const getUnderlying = symbol =>
   isOption(symbol) ? symbol.split('').filter(char => !nums.includes(char)).slice(0, -1).join('') : symbol
+
+
+const getExpiration = symbol =>
+  isOption(symbol) ?
+    symbol.replace(getUnderlying(symbol), '').split(/C|P/g)[0].split('').reduce((acc, x, index) =>
+      [1,3].includes(index) ? acc + x + '-' : acc + x, '20')
+    : null
+
+
+const getStrike = symbol => {
+  return isOption(symbol) ?
+    Number(symbol.replace(getUnderlying(symbol), '').split(/C|P/g)[1].split('').reduce((acc, x, index) =>
+      index === 4 ? acc + x + '.' : acc + x, ''))
+    : null
+}
+
 
 module.exports = {
   determineOptionTypeFromSymbol,
   isOption,
   getUnderlying,
+  getExpiration,
+  getStrike,
 }
--- utils/determineOptionType.test.js
+++ utils/determineOptionType.test.js
@@ -2,6 +2,8 @@ const {
   determineOptionTypeFromSymbol,
   isOption,
   getUnderlying,
+  getExpiration,
+  getStrike,
 } = require('./determineOptionType')
 
 describe('determineOptionTypeFromSymbol', () => {
@@ -65,5 +67,49 @@ describe('getUnderlying', () => {
   })
   it('Extracts TSLA', () => {
     expect(getUnderlying('TSLA211029P00885000')).toEqual('TSLA')
+  })
+})
+
+
+describe('getExpiration', () => {
+  it('Returns null if given non-option symbol', () => {
+    expect(getExpiration('AAPL')).toEqual(null)
+  })
+  it('NVTA211217P00015000', () => {
+    expect(getExpiration('NVTA211217P00015000')).toEqual('2021-12-17')
+  })
+  it('BB211203C00010500', () => {
+    expect(getExpiration('BB211203C00010500')).toEqual('2021-12-03')
+  })
+  it('SFIX220107C00019000', () => {
+    expect(getExpiration('SFIX220107C00019000')).toEqual('2022-01-07')
+  })
+  it('SOFI220107P00015500', () => {
+    expect(getExpiration('SOFI220107P00015500')).toEqual('2022-01-07')
+  })
+  it('TSLA220114C01260000', () => {
+    expect(getExpiration('TSLA220114C01260000')).toEqual('2022-01-14')
+  })
+})
+
+
+describe('getStrike', () => {
+  it('Returns null if given non-option symbol', () => {
+    expect(getStrike('AAPL')).toEqual(null)
+  })
+  it('NVTA211217P00015000', () => {
+    expect(getStrike('NVTA211217P00015000')).toEqual(15)
+  })
+  it('BB211203C00010500', () => {
+    expect(getStrike('BB211203C00010500')).toEqual(10.5)
+  })
+  it('SFIX220107C00019000', () => {
+    expect(getStrike('SFIX220107C00019000')).toEqual(19)
+  })
+  it('SOFI220107P00015500', () => {
+    expect(getStrike('SOFI220107P00015500')).toEqual(15.5)
+  })
+  it('TSLA220114C00550000', () => {
+    expect(getStrike('TSLA220114C01260000')).toEqual(1260)
   })
 })
