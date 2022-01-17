a06c023ff5fe285bfdf8d030f200b3ad64ff907c
Mwein <mweinberg21@gmail.com>
2022-01-16 23:10:36 -0600

    Watchlist schema

---
+++ db_models/watchlistSchema.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const WatchlistSchema = mongoose.Schema({
+  symbol: {
+    type: String,
+    required: true,
+  },
+  maxPositions: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  call: {
+    enabled: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    targetDelta: {
+      type: Number,
+      required: true,
+      default: 0.3,
+      min: [0.1],
+      max: [1]
+    },
+    minStrikeMode: {
+      type: String,
+      required: true,
+      default: 'auto',
+      enum: [ 'auto', 'custom', 'off' ]
+    },
+    minStrike: {
+      type: Number,
+      required: false,
+    }
+  },
+  put: {
+    enabled: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    targetDelta: {
+      type: Number,
+      required: true,
+      default: 0.3,
+      min: [0.1],
+      max: [1]
+    },
+  }
+})
+
+module.exports = mongoose.model('watchlist', WatchlistSchema)
