029e09b74e209038b955f0d4068756c6b024cc2e
Mwein <mweinberg21@gmail.com>
2022-01-30 19:25:23 -0600

    Continue on fail function

--- tradier/nextStrikeExpirations.js
+++ tradier/nextStrikeExpirations.js
@@ -1,6 +1,38 @@
 const network = require('../utils/network')
 const logUtil = require('../utils/log')
 
+// Failover function
+const _nextStrikeDates = (maxWeeksOut = 4) => {
+  const date = new Date()
+  const dates = []
+
+  while(dates.length < maxWeeksOut) {
+    switch (date.getDay()) {
+    case 0: date.setDate(date.getDate() + 5)
+      break
+    case 1: date.setDate(date.getDate() + 4)
+      break
+    case 2: date.setDate(date.getDate() + 3)
+      break
+    case 3: date.setDate(date.getDate() + 2)
+      break
+    case 4: date.setDate(date.getDate() + 1)
+      break
+    case 5: date.setDate(date.getDate() + 7) // Off to next friday
+      break
+    case 6: date.setDate(date.getDate() + 6)
+      break
+    }
+    // ISO string returns zulu time and can screw up the date
+    const offset = date.getTimezoneOffset()
+    const actualDate = new Date(date.getTime() - (offset*60*1000))
+    dates.push(actualDate.toISOString().split('T')[0])
+  }
+
+  return dates
+}
+
+
 const nextStrikeExpirations = async (symbol, limit = 2) => {
   try {
     if (limit === 0) {
@@ -27,11 +59,12 @@ const nextStrikeExpirations = async (symbol, limit = 2) => {
       message: e.toString()
     })
 
-    return []
+    return _nextStrikeDates(limit)
   }
 }
 
 
 module.exports = {
+  _nextStrikeDates,
   nextStrikeExpirations
 }
