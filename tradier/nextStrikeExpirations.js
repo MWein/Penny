f05f60d5bcc4a053dd030610cd8eba5b93c58241
Mwein <mweinberg21@gmail.com>
2022-01-30 19:16:54 -0600

    Tradier next expirations function

---
+++ tradier/nextStrikeExpirations.js
@@ -0,0 +1,36 @@
+const network = require('../utils/network')
+const logUtil = require('../utils/log')
+
+const nextStrikeExpirations = async (symbol, limit = 2) => {
+  try {
+    if (limit === 0) {
+      return []
+    }
+
+    const url = `/v1/markets/options/expirations?symbol=${symbol}`
+    const response = await network.get(url)
+    
+    // Tradier has this annoying tendency to return a single object rather than an array if there is one return value
+    // This should never happen with expirations, so I'm not doing the usual Array.isArray thing
+    const dates = response.expirations.date
+
+    // These symbols have multiple expirations per week
+    if ([ 'SPY', 'IWM', 'QQQ' ].includes(symbol)) {
+      return [ dates[0] ]
+    }
+
+    return dates.slice(0, limit)
+  } catch (e) {
+    logUtil.log({
+      type: 'error',
+      message: e.toString()
+    })
+
+    return []
+  }
+}
+
+
+module.exports = {
+  nextStrikeExpirations
+}
---
+++ tradier/nextStrikeExpirations.test.js
@@ -0,0 +1,106 @@
+const networkUtil = require('../utils/network')
+const logUtil = require('../utils/log')
+
+const {
+  nextStrikeExpirations
+} = require('./nextStrikeExpirations')
+
+
+describe('nextStrikeExpirations', () => {
+  beforeEach(() => {
+    networkUtil.get = jest.fn()
+    logUtil.log = jest.fn()
+  })
+
+  it('Logs and returns empty array if an error occurs', async () => {
+    networkUtil.get.mockImplementation(() => {
+      throw new Error('Damn')
+    })
+    const result = await nextStrikeExpirations('AAPL')
+    expect(result).toEqual([])
+    expect(logUtil.log).toHaveBeenCalledWith({
+      type: 'error',
+      message: 'Error: Damn'
+    })
+  })
+
+  it('Does not make any calls and returns empty array if limit is 0', async () => {
+    const result = await nextStrikeExpirations('AAPL', 0)
+    expect(result).toEqual([])
+    expect(networkUtil.get).not.toHaveBeenCalled()
+  })
+
+  it('Calls with the correct URL', async () => {
+    networkUtil.get.mockReturnValue({
+      expirations: {
+        date: [
+          '2021-01-01',
+          '2022-01-01'
+        ]
+      }
+    })
+    await nextStrikeExpirations('AAPL', 1)
+    expect(networkUtil.get).toHaveBeenCalledWith('/v1/markets/options/expirations?symbol=AAPL')
+  })
+
+  it('Returns only the next expiration regardless of the limit if given SPY, QQQ, or IWM', async () => {
+    networkUtil.get.mockReturnValue({
+      expirations: {
+        date: [
+          '2021-01-01',
+          '2022-01-01'
+        ]
+      }
+    })
+
+    const spyResult = await nextStrikeExpirations('SPY', 50)
+    expect(spyResult).toEqual([ '2021-01-01' ])
+
+    const iwmResult = await nextStrikeExpirations('IWM', 50)
+    expect(iwmResult).toEqual([ '2021-01-01' ])
+
+    const qqqResult = await nextStrikeExpirations('QQQ', 50)
+    expect(qqqResult).toEqual([ '2021-01-01' ])
+  })
+
+  it('By default, only returns the first 2 expirations', async () => {
+    networkUtil.get.mockReturnValue({
+      expirations: {
+        date: [
+          '2021-01-01',
+          '2022-01-01',
+          '2023-01-01',
+          '2024-01-01',
+        ]
+      }
+    })
+    const result = await nextStrikeExpirations('AAPL')
+    expect(result).toEqual([
+      '2021-01-01',
+      '2022-01-01',
+    ])
+  })
+
+  it('Returns number of expirations up to the limit', async () => {
+    networkUtil.get.mockReturnValue({
+      expirations: {
+        date: [
+          '2021-01-01',
+          '2022-01-01',
+          '2023-01-01',
+          '2024-01-01',
+          '2025-01-01',
+          '2026-01-01',
+        ]
+      }
+    })
+    const result = await nextStrikeExpirations('AAPL', 5)
+    expect(result).toEqual([
+      '2021-01-01',
+      '2022-01-01',
+      '2023-01-01',
+      '2024-01-01',
+      '2025-01-01',
+    ])
+  })
+})
