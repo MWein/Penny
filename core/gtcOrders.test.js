a7690cb5d0d98faed3c70bc66fc6747adda4f13a
Mwein <mweinberg21@gmail.com>
2022-01-31 19:24:15 -0600

    gtcOrders now ignores long options

--- core/gtcOrders.test.js
+++ core/gtcOrders.test.js
@@ -45,12 +45,60 @@ describe('_getOldOptionsPositions', () => {
     ])
   })
 
+  it('Filters out long calls', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 207.01),
+      generatePositionObject('AAPL', -1, 'call', 207.01),
+      generatePositionObject('AMZN', -2, 'put', 1870.71),
+      generatePositionObject('CAH', 203, 'stock', 50.41),
+    ]
+    const orders = []
+    const results = _getOldOptionsPositions(positions, orders)
+    expect(results).toEqual([
+      {
+        symbol: 'AAPL1234C3214',
+        quantity: 1,
+        costBasisPerPosition: 207.01,
+      },
+      {
+        symbol: 'AMZN1234P3214',
+        quantity: 2,
+        costBasisPerPosition: 935.36
+      },
+    ])
+  })
+
+  it('Filters out long puts', () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
+    const positions = [
+      generatePositionObject('AAPL', 1, 'put', 207.01),
+      generatePositionObject('AAPL', -1, 'call', 207.01),
+      generatePositionObject('AMZN', -2, 'put', 1870.71),
+      generatePositionObject('CAH', 203, 'stock', 50.41),
+    ]
+    const orders = []
+    const results = _getOldOptionsPositions(positions, orders)
+    expect(results).toEqual([
+      {
+        symbol: 'AAPL1234C3214',
+        quantity: 1,
+        costBasisPerPosition: 207.01,
+      },
+      {
+        symbol: 'AMZN1234P3214',
+        quantity: 2,
+        costBasisPerPosition: 935.36
+      },
+    ])
+  })
+
   it('Filters out ones that were made in the same day', () => {
     jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
     const positions = [
-      generatePositionObject('AAPL', 1, 'call', 207.01, '2021-10-12T14:41:11.405Z'),
-      generatePositionObject('AMZN', 2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
-      generatePositionObject('CAH', 3, 'call', 50.41, '2021-10-12T17:05:44.674Z'),
+      generatePositionObject('AAPL', -1, 'call', 207.01, '2021-10-12T14:41:11.405Z'),
+      generatePositionObject('AMZN', -2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
+      generatePositionObject('CAH', -3, 'call', 50.41, '2021-10-12T17:05:44.674Z'),
     ]
     const orders = []
     const results = _getOldOptionsPositions(positions, orders)
@@ -66,9 +114,9 @@ describe('_getOldOptionsPositions', () => {
   it('Filters out any that already have orders to close; ignores sell_to_open orders', () => {
     jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
     const positions = [
-      generatePositionObject('AAPL', 1, 'call', 207.01, '2021-10-11T14:41:11.405Z'),
-      generatePositionObject('AMZN', 2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
-      generatePositionObject('CAH', 3, 'call', 50.41, '2021-10-09T17:05:44.674Z'),
+      generatePositionObject('AAPL', -1, 'call', 207.01, '2021-10-11T14:41:11.405Z'),
+      generatePositionObject('AMZN', -2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
+      generatePositionObject('CAH', -3, 'call', 50.41, '2021-10-09T17:05:44.674Z'),
     ]
     const orders = [
       generateOrderObject('AAPL', 1, 'call', 'buy_to_close', 'pending'),
@@ -92,9 +140,9 @@ describe('_getOldOptionsPositions', () => {
   it('Returns the number of positions not covered if the quantity doesnt match', () => {
     jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
     const positions = [
-      generatePositionObject('AAPL', 4, 'call', 207.01, '2021-10-11T14:41:11.405Z'),
-      generatePositionObject('AMZN', 2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
-      generatePositionObject('CAH', 3, 'call', 50.41, '2021-10-09T17:05:44.674Z'),
+      generatePositionObject('AAPL', -4, 'call', 207.01, '2021-10-11T14:41:11.405Z'),
+      generatePositionObject('AMZN', -2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
+      generatePositionObject('CAH', -3, 'call', 50.41, '2021-10-09T17:05:44.674Z'),
     ]
     const orders = [
       generateOrderObject('AAPL', 1, 'call', 'buy_to_close', 'pending'),
@@ -162,9 +210,9 @@ describe('gtcOrders', () => {
   it('If there are positions and none have GTCs, send em all', async () => {
     jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
     position.getPositions.mockReturnValue([
-      generatePositionObject('AAPL', 1, 'call', 207.01, '2021-10-12T14:41:11.405Z'),
+      generatePositionObject('AAPL', -1, 'call', 207.01, '2021-10-12T14:41:11.405Z'),
       generatePositionObject('AMZN', -2, 'call', 1870.70, '2018-08-08T14:42:00.774Z'),
-      generatePositionObject('CAH', 3, 'call', 50.41, '2021-10-09T17:05:44.674Z'),
+      generatePositionObject('CAH', -3, 'call', 50.41, '2021-10-09T17:05:44.674Z'),
     ])
     order.getOrders.mockReturnValue([])
     settings.getSetting.mockReturnValue(0.75)
